b23695837c91f50716715597af5f7256ad18e506
Janet Karpenske <janetkarpenske@gmail.com>
2020-12-01 09:40:41 -0800

    Add other files
    Co-authored-by: Joseph Karnafel <josephkarnafel@gmail.com>
    Co-authored by: Forrest Reiland <freiland@email.arizona.edu>

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+// For this component we don't need to wrap our code in <React.Fragment> because we are only returning one element!
+function Header(){
+  return (
+    <h1>Reddit 2.0</h1>
+  );
+}
+
+export default Header;
